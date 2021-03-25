a28e2f14016f886b4758c240fdbd5a0df14483b3
Ralph Gonzalez <ralphgonz@gmail.com>
2021-03-24 20:30:32 -0400

    set up all needed controllers and start populating view and css

---
+++ app/controllers/player_controller.rb
@@ -0,0 +1,13 @@
+class PlayerController < ApplicationController
+  # return view, initially hidden with 'loading' overlay
+  def init
+  end
+
+  # return initial user data
+  def username
+  end
+
+  # return additional purchase history data
+  def moredata
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "params#init"
 
   get "/params", to: "params#init"
+  get "/player", to: "player#init"
+  get "/username", to: "player#username"
+  get "/moredata", to: "player#moredata"
 end
