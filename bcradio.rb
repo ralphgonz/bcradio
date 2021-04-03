250b9b8c3ca5e700a6fb63a4b2d68e3d9e1f550f
Ralph Gonzalez <rgonzalez1@chathamfinancial.com>
2021-04-03 15:28:28 -0400

    fix rubocop complaints, permit empty history count

--- bcradio.rb
+++ bcradio.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # bcradio.rb
 require 'socket'
 require 'net/http'
@@ -6,15 +8,17 @@ require 'cgi'
 server = TCPServer.new 5678
 $stdout.sync = true
 
+# Http response wrapper
 class Response
   attr_reader :code
 
-  def initialize(code:, data: "")
-    @response =
-    "HTTP/1.1 #{code}\r\n" +
-    "Content-Length: #{data.size}\r\n" +
-    "\r\n" +
-    "#{data}\r\n"
+  def initialize(code:, data: '')
+    @response = <<~END_OF_STRING
+      HTTP/1.1 #{code}
+      Content-Length: #{data.size}
+      
+      #{data}
+    END_OF_STRING
     @code = code
   end
 
@@ -23,39 +27,35 @@ class Response
   end
 end
 
+# Main BcRadio server
 class BcRadio
   def initialize session
     @session = session
     @query = nil
-    @responseData = nil
+    @response_data = nil
+    @params = {}
     request = session.gets
     return if request.nil?
-    method, path = request.split(/\s/)
-    return if path.nil?
-    path, paramString = path.split('?')
-    @params = paramString.nil? ? {} : CGI::parse(paramString)
-    _, @query = path.split('/', 2)
-    if @query.nil? || @query.empty?
-      @query = "index.html"
-    end
+
+    _method, uri = request.split(/\s/)
+    parse_uri uri
   end
 
   def process_request
     case @query
-    when nil
-      return
-    when "moredata"
-      @responseData = handle_more_data_request
-    when /^userdata\/(.+)/
-      @responseData = handle_user_data_request $1
-    else
-      @responseData = handle_file_request
-    end    
+    when %r{^userdata/(.+)}
+      @response_data = handle_user_data_request Regexp.last_match(1)
+    when 'moredata'
+      @response_data = handle_more_data_request
+    when /.+/
+      @response_data = handle_file_request
+    end
   end
 
   def send_response
-    return unless @responseData
-    response = Response.new(code: 200, data: @responseData)
+    return unless @response_data
+
+    response = Response.new(code: 200, data: @response_data)
     response.send(@session)
   end
 
@@ -63,47 +63,59 @@ class BcRadio
     @session.close
   end
 
+  ##############################################################################
   private
+
+  def parse_uri uri
+    return if uri.nil?
+
+    uri, param_string = uri.split('?')
+    @params = param_string.nil? ? {} : CGI.parse(param_string)
+    _, @query = uri.split('/', 2)
+    @query = 'index.html' if @query.nil? || @query.empty?
+  end
 
   def handle_file_request
     File.binread(@query)
   end
 
-  def handle_user_data_request userName
-    puts "User: #{userName} at #{Time.now}"
-    uri = URI("https://bandcamp.com/#{userName}")
+  def handle_user_data_request user_name
+    puts "User: #{user_name} at #{Time.now}"
+    uri = URI("https://bandcamp.com/#{user_name}")
     Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-      bcRequest = Net::HTTP::Get.new(uri.request_uri)
-      if @params.key?("identity-cookie")
-        bcRequest['Cookie'] = "identity=#{CGI.escape(@params['identity-cookie'].first())}"
+      bc_request = Net::HTTP::Get.new(uri.request_uri)
+      if @params.key?('identity-cookie')
+        bc_request['Cookie'] = "identity=#{CGI.escape(@params['identity-cookie'].first)}"
       end
-      http.request(bcRequest).body
+      http.request(bc_request).body
     end
   end
 
   def handle_more_data_request
-    uri = URI("https://bandcamp.com/api/fancollection/1/collection_items")
+    uri = URI('https://bandcamp.com/api/fancollection/1/collection_items')
     Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-      bcRequest = Net::HTTP::Post.new(uri.request_uri)
-      if @params.key?("identity-cookie")
-        bcRequest['Cookie'] = "identity=#{CGI.escape(@params['identity-cookie'].first())}"
+      bc_request = Net::HTTP::Post.new(uri.request_uri)
+      if @params.key?('identity-cookie')
+        bc_request['Cookie'] = "identity=#{CGI.escape(@params['identity-cookie'].first)}"
       end
-      bcRequest['Content-Type'] = "application/json"
-      bcRequest.body = "{\"fan_id\":#{@params['fan-id'].first()},\"older_than_token\":\"#{@params['older-than-token'].first()}\",\"count\":#{@params['count'].first()}}"
-      http.request(bcRequest).body
+      bc_request['Content-Type'] = 'application/json'
+      bc_request.body = "{\"fan_id\":#{@params['fan-id'].first},"\
+        "\"older_than_token\":\"#{@params['older-than-token'].first}\","\
+        "\"count\":#{@params['count'].first.to_i}}"
+      http.request(bc_request).body
     end
   end
 end
 
-while session = server.accept
+while (session = server.accept)
   begin
-    bcRadio = BcRadio.new session
-    bcRadio.process_request
-  rescue => e
+    bc_radio = BcRadio.new session
+    bc_radio.process_request
+  rescue => e # rubocop:disable Style/RescueStandardError
     puts "==== #{e.message} at #{Time.now}"
   else
-    bcRadio.send_response  
+    bc_radio.send_response
   ensure
-    bcRadio.close    
+    bc_radio.close
   end
 end
