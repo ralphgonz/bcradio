86edbd885d46eb777cd940f49633ab4f0f28368f
Ralph Gonzalez <rgonzalez1@chathamfinancial.com>
2021-06-02 00:47:26 -0400

    test

--- bcradio.rb
+++ bcradio.rb
@@ -74,8 +74,8 @@ class BcRadio
 
   def send_redirect
     puts "==== Sending 301 redirect at #{Time.now}" if $verbose
-    data = "Location: https://#{@headers['host']}#{@uri}"
-    response = Response.new(code: 301, data: data)
+    # data = "Location: https://#{@headers['host']}#{@uri}"
+    response = Response.new(code: 301, data: 'Location: https://bcradio.muskratworks.com')
     response.send(@session)
   end
 
